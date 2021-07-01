a38af13b5b08a72c770c5d236d760f96530498c2
LouisLU9911 <louislu9911@gmail.com>
2021-07-02 00:13:40 +0800

    add lcp 7

---
+++ lcp/lcp-7-chuan-di-xin-xi.go
@@ -0,0 +1,35 @@
+package lcp
+
+func numWays(n int, relation [][]int, k int) int {
+    mat := make([][]int, n)
+    for i := 0; i < n; i++ {
+        mat[i] = make([]int, n)
+    }
+
+    for i := 0; i < len(relation); i++ {
+        mat[relation[i][0]][relation[i][1]] = 1
+    }
+
+    row := make([]int, n)
+    for i := 0; i < n; i++ {
+        row[i] = mat[0][i]
+    }
+
+    res := make([]int, n)
+    for i := 0; i < k-1; i++ {
+
+        for j := 0; j < n; j++ {
+            sum := 0
+            for k := 0; k < n; k++ {
+                sum += row[k] * mat[k][j]
+            }
+            res[j] = sum
+        }
+
+        for j := 0; j < n; j++ {
+            row[j] = res[j]
+        }
+
+    }
+    return row[n-1]
+}
