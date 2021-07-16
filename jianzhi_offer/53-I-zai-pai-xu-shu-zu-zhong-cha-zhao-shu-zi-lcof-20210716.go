40aeea0e6fd036ae3c291098254c9157423c82a1
LouisLU9911 <louislu9911@gmail.com>
2021-07-17 00:06:12 +0800

    add jianzhi_offer 53-I && fix end of file format

---
+++ jianzhi_offer/53-I-zai-pai-xu-shu-zu-zhong-cha-zhao-shu-zi-lcof-20210716.go
@@ -0,0 +1,10 @@
+package jianzhi_offer
+
+func search(nums []int, target int) int {
+    leftmost := sort.SearchInts(nums, target)
+    if leftmost == len(nums) || nums[leftmost] != target {
+        return 0
+    }
+    rightmost := sort.SearchInts(nums, target + 1) - 1
+    return rightmost - leftmost + 1
+}
