1b63eb67a4bb8e9c5162e40aba4edbc9b44a6893
LouisLU9911 <louislu9911@gmail.com>
2021-07-18 19:29:04 +0800

    fix go format

--- mianshijindian/10-02-group-anagrams-lcci-20210718.go
+++ mianshijindian/10-02-group-anagrams-lcci-20210718.go
@@ -1,26 +1,28 @@
 package mianshijindian
 
+import "sort"
+
 func groupAnagrams(strs []string) [][]string {
-    words := make(map[string][]string)
-    for _, str := range(strs) {
-        strSorted := sortWord(str)
-        if _, ok := words[strSorted]; ok {
-            words[strSorted] = append(words[strSorted], str)
-        } else {
-            words[strSorted] = []string{str}
-        }
-    }
-    ret := [][]string{}
-    for _, v := range(words) {
-        ret = append(ret, v)
-    }
-    return ret
+	words := make(map[string][]string)
+	for _, str := range strs {
+		strSorted := sortWord(str)
+		if _, ok := words[strSorted]; ok {
+			words[strSorted] = append(words[strSorted], str)
+		} else {
+			words[strSorted] = []string{str}
+		}
+	}
+	ret := [][]string{}
+	for _, v := range words {
+		ret = append(ret, v)
+	}
+	return ret
 }
 
 func sortWord(word string) string {
-    wordBytes := []byte(word)
-    sort.Slice(wordBytes, func (i, j int) bool {
-        return wordBytes[i] < wordBytes[j]
-    })
-    return string(wordBytes)
+	wordBytes := []byte(word)
+	sort.Slice(wordBytes, func(i, j int) bool {
+		return wordBytes[i] < wordBytes[j]
+	})
+	return string(wordBytes)
 }
