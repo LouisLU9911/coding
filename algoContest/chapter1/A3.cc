3799333185dadf525042f214c5a247162defaddf
Louis LU <louislu9911@gmail.com>
2021-12-20 08:30:09 +0800

    feat: add algoContest c1 exams

---
+++ algoContest/chapter1/A3.cc
@@ -0,0 +1,9 @@
+#include <cstdio>
+#include <cmath>
+
+int main() {
+  printf("%d\n", sqrt(-10));
+  printf("%f\n", sqrt(-10));
+  return 0;
+}
+
---
+++ algoContest/chapter1/A4.cc
@@ -0,0 +1,9 @@
+#include <cstdio>
+
+int main() {
+  printf("%d\n", 1.0/0.0);
+  printf("%f\n", 1.0/0.0);
+  printf("%d\n", -1.0/0.0);
+  printf("%f\n", -1.0/0.0);
+  return 0;
+}
